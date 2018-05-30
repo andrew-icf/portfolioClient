e29d3745d49213a2c9b7d72cae69aa407da8ff83
Andrew Vandermeer <andrew.vandermeer@mbp-1shtdf.home>
2018-05-29 20:55:19 -0600

    Added Briebug and Blndspt to projects page

---
+++ src/components/blndspt.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Blndspt extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Blndspt.com</h3>
+        <div className='blndspt'>
+          <p>I have been a dedicated resource for Hawaiian Airlines and the release of their mobile application.</p>
+          <table>
+            <tr>
+              <th><img src='../../img/HA.png' /></th>
+            </tr>
+            <tr>
+              <td><p>I have been working with the developers automating and creating the tests for the new API.</p></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
+
 import Weather from '../weather/weather';
+import Briebug from './briebug';
+import Blndspt from './blndspt';
 
 export default class Projects extends Component {
   render() {
     return(
       <div>
-        <div className='projects'>
-          <h1 className="mainHeader">Projects</h1>
-          <h3>BrieBug Solutions</h3>
-          <section>
-            <p>These are projects that I worked on when I was at Briebug Solutions</p>
-            <article>
-              <img src='../../img/Briebug.png' />
-              <p>I worked with general fixes and updates within the company.</p>
-            </article>
-            <article>
-              <img src='../../img/nextplane.png' />
-              <p>I worked with general fixes and updates within the company.</p>
-            </article>
-            <article>
-              <img src='../../img/rme.png' />
-              <p>I worked with general fixes and updates within the company.</p>
-            </article>
-            <article>
-              <img src='../../img/streetsweep.png' />
-              <p>I worked with general fixes and updates within the company.</p>
-            </article>
-            <article>
-              <img src='../../img/sizet.png' />
-              <p>I worked with general fixes and updates within the company.</p>
-            </article>
-          </section>
-        </div>
-        <div>
-          <Weather />
-        </div>
+        <h1 className="mainHeader">Projects</h1>
+        <Blndspt />
+        <Briebug />
+        <Weather />
       </div>
     );
   }
